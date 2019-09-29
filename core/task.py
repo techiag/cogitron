c49983fb36a6d1f2dad92adc3f79dbfef1cdd6b5
Jontahan <jona.jorg@gmail.com>
2019-09-29 14:57:02 +0200

    Add execution loop and shutdown task

--- core/task.py
+++ core/task.py
@@ -1,13 +1,28 @@
 from abc import ABC, abstractmethod
+from timeit import default_timer as timer
+import time
 
 class Task(ABC):
-    def __init__(self, com):
-        self.com = com
+    def __init__(self, cogref):
+        self.cogref = cogref
+        self.exec_time = -1
+
+    def execute(self):
+        start = timer()
+        self.exec_body()
+        end = timer()
+        self.exec_time = end - start 
 
     @abstractmethod
-    def execute(self):
+    def exec_body(self):
         pass
 
 class TaskDriveForward(Task):
-    def execute(self):
+    def exec_body(self):
         print('Driving forward!')
+        time.sleep(1)
+
+class TaskShutdown(Task):
+    def exec_body(self):
+        print('Shutting down.')
+        self.cogref.shutdown = True
--- main.py
+++ main.py
@@ -3,12 +3,28 @@ from core.task import *
 #from com import Interface
 
 class Cogitron:
-    pass
+    def __init__(self, emulate_hardware=False, verbose=False):
+        self.shutdown = False
+        self.verbose = verbose
+        self.com = None
+        self.tasks = Queue()
+        self.tasks.put(TaskDriveForward(self))
+        self.tasks.put(TaskDriveForward(self))
+        self.tasks.put(TaskDriveForward(self))
+        self.tasks.put(TaskShutdown(self))
+        
+    def run(self):
+        while not self.shutdown:
+            if self.tasks.empty():
+                print('Task queue is empty, sleeping...')
+                time.sleep(1.0)
+            else:
+                t = self.tasks.get()
+                t.execute()
+
+                if self.verbose: print('Task execution time: {:.4f}s'.format(t.exec_time))
 
 if __name__ == '__main__':
     print('Initializing Cogitron...')
-    com = None
-    tasks = Queue()
-    tasks.put(TaskDriveForward('Take over the world'))
-    t = tasks.get()
-    t.execute()
+    cogitron = Cogitron(verbose=True)
+    cogitron.run()
