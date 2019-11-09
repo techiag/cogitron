e32236152964ae2fdd2d9b424a5a46d0dac4af30
ericy <ericy@stud.ntnu.no>
2019-11-09 17:30:58 +0100

    restructured Communicator class

--- com/communicator.py
+++ com/communicator.py
@@ -1,41 +1,60 @@
-import fakeSerial as emu
+#import com.fakeserial as emu
 import serial as real
 import time
 
-
 class Communicator(object):
     def __init__(self, communicator):
+        '''Sets up either emulator serial or serial connection
+        to real devices'''
+
         if communicator == "emu":
-            self.com = emu.Serial(9600)
+            pass
+            #self.com = emu.Serial(9600)
         elif communicator == "real":
-            self.com = real.Serial("/dev/tty.usbmodem14101", 9600)
+            self.con_nodes = []
+            self.deviceHash = {"Motor" : 0, "Sensor" : 1}
 
-    def sendMessage(self, message):
+            '''Create a connection to each slave'''
+            self.motor = real.Serial("/dev/cu.usbmodem14101", 9600)
+            self.sensor = real.Serial("/dev/tty.usbmodem14201", 9600)
+
+            '''Add connected slaves to list'''
+            self.con_nodes.append(self.motor)
+            self.con_nodes.append(self.sensor)
+
+    def getConnection(self, string):
+        '''Return the right connection based on string'''
+        if (string.lower() == "motor"):
+            return self.con_nodes[self.deviceHash["Motor"]]
+        elif (string.lower() == "sensor"):
+            return self.con_nodes[self.deviceHash["Sensor"]]
+
+
+    def sendMessage(self,connection, message):
         messageToBytes = bytes(message, 'utf-8')
-        self.com.write(messageToBytes)
+        connection.write(messageToBytes)
 
-
-    def receiveMessage(self):
+    def receiveMessage(self, connection):
         messageReceived = ""
-        
-        time.sleep(0.5) # in_waiting won't work without this time sleep.
-       
-        while self.com.in_waiting >0:
-            byteReceived = ord(self.com.read())
+
+        time.sleep(0.5)  # in_waiting won't work without this time sleep.
+
+        while connection.in_waiting > 0:
+            byteReceived = ord(connection.read())
+            #print(byteReceived)
             letter = chr(byteReceived)
             if(letter != ""):
                 messageReceived += letter
         return messageReceived
 
-
 if __name__ == "__main__":
-    '''The emulator is not working, especially because their is not attribute in_waiting, to tell the buffer size
-    But! Commmunication seems to be working both ways'''
+    '''The emulator is not working, especially because there is not attribute in_waiting in emulator, to tell the buffer size
+    Real serial connection works'''
 
-    bob = Communicator("real") #instance of Communicator
-    messageOut = "driveForward" #the message to send to arduino
-    
+    bob = Communicator("real")  # instance of Communicator
+    messageOut = "setMotors;1;2;2"  # the message to send to arduino
+
     while True:
-        print(bob.receiveMessage()) 
+        print(bob.receiveMessage(bob.getConnection("motor")))
+        messageOut = input("\nenter command\n")
         bob.sendMessage(messageOut)
-
