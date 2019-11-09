01754b4db4f871afaf897a8f1ccd382511fb6371
ericy <ericy@stud.ntnu.no>
2019-11-09 19:01:31 +0100

    restructured communicator

--- com/communicator.py
+++ com/communicator.py
@@ -1,26 +1,23 @@
-#import com.fakeserial as emu
+import fakeserial as emu
 import serial as real
 import time
 
 class Communicator(object):
-    def __init__(self, communicator, motorUSB, sensorUSB):
+    def __init__(self, motorUSB="emu", sensorUSB="emu"):
         '''Sets up either emulator serial or serial connection
         to real devices'''
+    
+        self.con_nodes = []
+        self.deviceHash = {"MotorSerial" : 0, "SensorSerial" : 1}
 
-        if communicator == "emu":
-            pass
-            #self.com = emu.Serial(9600)
-        elif communicator == "real":
-            self.con_nodes = []
-            self.deviceHash = {"MotorSerial" : 0, "SensorSerial" : 1}
+        '''Create a connection to each slave'''
+        self.motor_serial = real.Serial(motorUSB, 9600) if motorUSB is not "emu" else emu.Serial(9600)
+        self.sensor_serial = real.Serial(sensorUSB, 9600)if motorUSB is not "emu" else emu.Serial(9600)
 
-            '''Create a connection to each slave'''
-            self.motor_serial = real.Serial(motorUSB, 9600)
-            self.sensor_serial = real.Serial(sensorUSB, 9600)
+        '''Add connected slaves to list'''
+        self.con_nodes.append(self.motor_serial)
+        self.con_nodes.append(self.sensor_serial)
 
-            '''Add connected slaves to list'''
-            self.con_nodes.append(self.motor_serial)
-            self.con_nodes.append(self.sensor_serial)
 
     def getConnection(self, string):
         '''Return the right connection based on string'''
@@ -35,15 +32,6 @@ class Communicator(object):
         connection.write(messageToBytes)
 
     def receiveMessage(self, connection):
-        messageReceived = ""
-
-        time.sleep(0.5)  # in_waiting won't work without this time sleep.
-
-        while connection.in_waiting > 0:
-            byteReceived = ord(connection.read())
-            #print(byteReceived)
-            letter = chr(byteReceived)
-            if(letter != ""):
-                messageReceived += letter
+        messageReceived = str(connection.readline()[:-2])
         return messageReceived
 
