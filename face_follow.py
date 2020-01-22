c2d64e3058f5b1c5e91745fcfae12c83ebeffa79
Cogitron <cogitron@cogitron.ai>
2020-01-22 19:16:15 +0000

    Add facial follow scripts (Pi version)

--- face_follow.py
+++ face_follow.py
@@ -1,13 +1,17 @@
 from io import BytesIO
-#from picamera import PiCamera
+from picamera import PiCamera
+from cogitron import Cogitron
 import cv2
 import numpy as np
 import os
+import time
+import math
 
 # set to true if captures are too slow
 # the video port bypasses some postprocessing, sacrificing quality for framerate
 USE_VIDEO_PORT = False
-RESOLUTION = (1296, 972) # (width, height). use a 4:3 resolution for max FOV
+#RESOLUTION = (1296, 972) # (width, height). use a 4:3 resolution for max FOV
+RESOLUTION = (640, 480)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 face_cascades = cv2.CascadeClassifier(os.path.join(PROJECT_ROOT, "cascades/data/haarcascade_frontalface_alt2.xml"))
@@ -23,8 +27,9 @@ class Sauron:
 
         self.convolution_nn = convolution_nn
         #self.cam = PiCamera()
-        self.cap = cv2.VideoCapture(0)
-        #self.cam = PiCamera(resolution=RESOLUTION)
+        #self.cap = cv2.VideoCapture(0)
+        self.cam = PiCamera(resolution=RESOLUTION)
+        self.cam.rotation = 180
         self.buf = np.empty(shape, dtype=np.uint8)
 
     def recoginze(self):
@@ -34,13 +39,50 @@ class Sauron:
         print(faces)
         return faces
 
-    #def recognize_pi(self):
-        #cam.capture(self.buf, format="bgr", use_video_port=USE_VIDEO_PORT)
-        #frame = buf[:RESOLUTION[1],:RESOLUTION[0]] # slice off padding pixels
-        #assert frame.base is buf # make sure slicing doesn't copy
-        #value = self.convolution_nn.recoginze(frame)
-        #print(value)
-        #return value
+    def recognize_pi(self):
+        self.cam.capture(self.buf, format="bgr", use_video_port=USE_VIDEO_PORT)
+        frame = self.buf[:RESOLUTION[1],:RESOLUTION[0]] # slice off padding pixels
+        assert frame.base is self.buf # make sure slicing doesn't copy
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        value = face_cascades.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+        print(value)
+        return value
 
 sr = Sauron()
-sr.recoginze()
+center = (RESOLUTION[0] / 2, RESOLUTION[1] / 2)
+yes = 45
+no = 90
+STEP = 5
+cogitron = Cogitron()
+resetcount = 10
+while True:
+    time.sleep(0.1)
+    faces = sr.recognize_pi()
+    if len(faces) == 0:
+        resetcount -= 1
+        if resetcount <= 0:
+            yes = 45
+            no = 90
+    else:
+        resetcount = 10
+    nearest = (0, 0)
+    nearest_dist = 1000000
+    for face in faces:
+        pos = (face[0] + face[2] / 2 - center[0], face[1] + face[3] / 2 - center[1])
+        dist = pos[0] ** 2 + pos[1] ** 2
+        if dist < nearest_dist:
+            nearest = pos
+            nearest_dist = dist
+    print(nearest)
+    if nearest[0] < 0:
+        no = min(no + 5, 135)
+    elif nearest[0] > 0:
+        no = max(no - 5, 45)
+    if nearest[1] > 0:
+        yes = min(yes + 5, 70)
+    elif nearest[1] < 0:
+        yes = max(yes - 5, 40)
+    cogitron.com.sendMessage(cogitron.com.motor_serial, "moveServo;0;" + str(no))
+    time.sleep(0.1)
+    cogitron.com.sendMessage(cogitron.com.motor_serial, "moveServo;1;" + str(yes))
+
